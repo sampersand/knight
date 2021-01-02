2d34d161ce985802ab1de30cf538c8dc76cb4dbf
Sam Westerman <sam.westerman@usaepay.com>
2021-01-02 00:02:37 -0800

    added ruby

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,57 +1,16 @@
 require_relative 'value'
-
-exit
-module Kn
-	class Value < Kn
-	end
-end
-
-__END__
-
-#ifndef KN_AST_H
-#define KN_AST_H
-
-#include "env.h"
-#include "value.h"
-
-/*
- * The type that represents executable code within Knight.
- *
- * This type is created via `kn_ast_parse` and
- * should be disposed of via `kn_ast_free`.
- */
-struct kn_ast_t;
-
-// The "stream" type.
-typedef const char ** stream_t;
-
-/*
- * Parse a `kn_ast_t` from an input stream.
- *
- * Returns `NULL` if the stream is at an end, and `die`s if an error occurs
- * whilst reading the stream.
- */
-struct kn_ast_t *kn_ast_parse(stream_t);
-
-/*
- * Runs an `kn_ast_t`, returning the value associated with it.
- * 
- * If any errors occur whilst running the tree, the function will abort the
- * program with a message indicating the error.
- */
-struct kn_value_t kn_ast_run(const struct kn_ast_t *);
-
-/*
- * Clones an `kn_ast_t`.
- */
-struct kn_ast_t *kn_ast_clone(const struct kn_ast_t *);
-
-/*
- * Releases all the resources the `kn_ast_t` has associated with it.
- */
-void kn_ast_free(struct kn_ast_t *);
-
-// Debugging function used to inspect an AST.
-void kn_ast_dump(const struct kn_ast_t *);
-
-#endif /* KN_AST_H */
+Kn::Value.parse(<<EOS).run
+; = fizzbuzz BLOCK
+	; = n 0
+	: WHILE < (= n + 1 n) (+ max 1)
+		: OUTPUT
+			: IF ! (% n 15)
+				: "FizzBuzz"
+			: IF ! (% n 5)
+				: "Fizz"
+			: IF ! (% n 3)
+				: "Buzz"
+				: n
+; = max 100
+: CALL fizzbuzz
+EOS
--- ruby/value.rb
+++ ruby/value.rb
@@ -1,79 +1,123 @@
+require_relative 'function'
 module Kn
-	class Value
-		include Enumerable
+	ENVIRONMENT = {}
 
-		def self.new(data, ...)
+	class Value
+		include Comparable
+		
+		attr_reader :data
+
+		def self.parse(stream)
+			nil until stream.slice!(/\A(?:#.*?(\n|\z)|\A[\s()\[\]{}:])*/).empty?
+
+			[Number, Text, Boolean, Identifier, Null, Ast]
+				.lazy
+				.filter_map { _1.parse(stream) }
+				.first or abort "unknown character start #{stream[0].inspect}"
+		end
+
+		def self.new(value, ...)
 			return super unless self == Value
 
-			case data
-			when ::String then String.new(data, ...)
-			when Numeric then Number.new(data, ...)
-			when true, false then Boolean.new(data, ...)
-			when nil then Null.new(...)
-			else raise ArgumentError, "unknown data '#{data.inspect}."
+			case value
+			when Value       then value
+			when String      then Text.new value
+			when Numeric     then Number.new value.to_i
+			when true, false then Boolean.new value
+			when nil         then Null.new value
+			else             raise ArgumentError, "no conversion found for #{value.class} to Value."
 			end
 		end
 
-		def initialize(value) = @value = value
-		def inspect = "#{self.class}(#{@value.inspect})"
+		def initialize(data) = @data = data
+		def inspect = "#{self.class}(#{@data.inspect})"
 
-		def run = @value
+		alias run itself
 
-		def to_s = @value.to_s
-		def to_i = @value.to_i
+		def to_s = run.data.to_s
+		def to_i = run.data.to_i
+		def to_b = run.to_b
 
-		%i(+ - * / % ^ <=>).each do 
-		def +(*) = abort "method '#{__method__}' not defined for #{inspect}"
-		def -(*) = abort "method '#{__method__}' not defined for #{inspect}"
-		def *(*) = abort "method '#{__method__}' not defined for #{inspect}"
-		def /(*) = abort "method '#{__method__}' not defined for #{inspect}"
-		def %(*) = abort "method '#{__method__}' not defined for #{inspect}"
-		def ^(*) = abort "method '#{__method__}' not defined for #{inspect}"
-		def <=>(*) = abort "method '#{__method__}' not defined for #{inspect}"
+		def ! = !to_b
 
-		def ! = Number.new(!to_b)
-		def &(rhs) = to_b ? rhs : self
-		def |(rhs) = to_b ? self : rhs
+		def +(rhs) = to_i + rhs.to_i
+		def -(rhs) = to_i - rhs.to_i
+		def *(rhs) = to_i * rhs.to_i
+		def /(rhs) = to_i / rhs.to_i
+		def %(rhs) = to_i % rhs.to_i
+		def **(rhs) = to_i ** rhs.to_i
+		def <=>(rhs) = to_i <=> rhs.to_i
+
+		def hash = @data.hash
+		def ==(rhs) = rhs.is_a?(self.class) && @data == rhs.data
+		alias eql? ==
 	end
 
-	class String < Value
-		def to_b = !@value.empty?
+	class Number < Value
+		def self.parse(stream)
+			stream.slice!(/\A\d+/) and new $&.to_i
+		end
 
-		def +(rhs) = String.new("#{self}#{rhs}")
-		def *(rhs) = String.new(to_s * rhs.to_i)
-		def <=>(rhs) = Number.new(to_s <=> rhs.to_s)
+		def to_b = !@data.zero?
+	end
+
+	class Text < Value
+		def self.parse(stream)
+			case
+			when !stream.slice!(/\A(["'])(.*?)(\1|\z)/) then nil
+			when $3 then new $2
+			else raise ArgumentError, "unterminated string encountered"
+			end
+		end
+
+		def to_b = !@data.empty?
+
+		def +(rhs) = "#{self}#{rhs}"
+		def *(rhs) = @data * rhs.to_i
+		def <=>(rhs) = @data <=> rhs.to_s
 	end
 
 	class Boolean < Value
-		def to_b = @value
-		def to_i = @value ? 1 : 0
+		def self.parse(stream)
+			stream.slice!(/\A([TF])[A-Z]*/) and new $&.to_i
+		end
 
-		def <=>(rhs) = Number.new(to_b <=> rhs.to_b)
+		alias to_b data
+		def to_i = @data ? 1 : 0
 	end
 
 	class Null < Value
-		def self.new = super(nil)
-		def inspect = self.class.to_s
+		def self.parse(stream)
+			stream.slice!(/\AN[A-Z]*/) and new
+		end
+
+		def initialize = super(nil)
 
 		def to_s = "null"
 		def to_i = 0
 		def to_b = false
 	end
 
-	class Number < Value
-		def to_b = !@value.zero?
-
-		%i(+ - * / % ** <=>).each do |func|
-			define_method func do |rhs|
-				Number.new to_i.public_send(func, rhs.to_i)
-			end
+	class Identifier < Value
+		def self.parse(stream)
+			stream.slice!(/\A[a-z_][a-z0-9_]*/) and new $&
 		end
+
+		def run = Kn::ENVIRONMENT.fetch(@data){ raise KeyError, "unknown identifier '#@data'" }
 	end
 
 	class Ast < Value
-		KNOWN_METHODS =  
-		def method_missing(...) = @value.run.public_send(...)
-		def respond_to_missing?(...) = @value.run.public_send(...)
-end
+		def self.parse(stream)
+			func = Kn::Function[stream[0]] or return
+			stream.slice! /\A(?:[A-Z]+|.)/
+			new func, func.arity.times.map { Value.parse stream }
+		end
 
-require_relative 'ast' unless defined? Kn::Ast
+		def initialize(func, args)
+			@func, @args = func, args
+		end
+
+		def inspect = "#{self.class}(#@func, #{@args.map(&:inspect).join(', ')})"
+		def run = @func.(*@args)
+	end
+end
