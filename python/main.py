641c4e404af0c5ac8d9c17e2b94122f830470cd7
J87 <68929154+jake-87@users.noreply.github.com>
2021-02-16 19:57:14 +1100

    Added docs

--- python/main.py
+++ python/main.py
@@ -2,11 +2,11 @@ from knight import *
 
 import sys
 
-# lol
+# stop the program from getting stuck in an infinite loop
 sys.setrecursionlimit(10000)
 
 if len(sys.argv) != 3 or sys.argv[1] not in ['-e', '-f']:
-	quit(f"usage: {sys.argv[0]} [-e 'program'] [-f 'file']")
+	quit(f"usage: {sys.argv[0]} [-e 'program'] [-f 'file']") # proper usage
 
 if sys.argv[1] == '-e':
 	program = sys.argv[2]
@@ -14,4 +14,4 @@ else:
 	with open(sys.argv[2]) as f:
 		program = f.read()
 
-Value.parse(program).run()
+Value.parse(program).run() # parse the program
