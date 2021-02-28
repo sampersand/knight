0d02a665ecdfecaa9ff85cceca2fba89e37557b5
Sam Westerman <sam.westerman@usaepay.com>
2021-02-28 01:38:48 -0800

    added buckets, but i still have identifier errors

--- c/src/env.c
+++ c/src/env.c
@@ -4,6 +4,108 @@
 #include "env.h"    /* prototypes, kn_value_t, kn_value_free */
 #include "shared.h" /* xmalloc, xrealloc */
 
+
+struct kn_env_pair_t {
+	char *name;
+	struct kn_value_t value;
+};
+
+struct kn_env_bucket_t {
+	size_t capacity;
+	size_t length;
+	struct kn_env_pair_t *pairs;
+};
+
+#define NBUCKETS 256
+static struct kn_env_bucket_t BUCKETS[NBUCKETS];
+
+void kn_env_init(size_t capacity) {
+	assert(capacity <= SIZE_MAX / sizeof(struct kn_value_t));
+	assert(capacity != 0);
+
+	for (size_t i = 0; i < NBUCKETS; ++i) {
+		BUCKETS[i] = (struct kn_env_bucket_t) {
+			.capacity = capacity,
+			.length = 0,
+			.pairs = xmalloc(
+				capacity * sizeof(struct kn_env_pair_t))
+		};
+	}
+
+}
+
+void kn_env_free() {
+	struct kn_env_bucket_t *bucket;
+
+	for (size_t i = 0; i < NBUCKETS; ++i) {
+		bucket = &BUCKETS[i];
+
+		for (size_t len = 0; len < bucket->length; ++len) {
+			free(bucket->pairs[len].name);
+			kn_value_free(&bucket->pairs[len].value);
+		}
+
+		free(bucket->pairs);
+		bucket->length = 0;
+		bucket->capacity = 0;
+	}
+}
+
+static struct kn_env_bucket_t *get_bucket(const char *identifier) {
+	assert(identifier != NULL);
+	return &BUCKETS[0];
+	// return NULL;
+}
+
+static struct kn_env_pair_t *get_pair(
+	const struct kn_env_bucket_t *bucket,
+	const char *identifier
+) {
+	for (size_t i = 0; i < bucket->length; ++i) {
+		if (strcmp(bucket->pairs[i].name, identifier) == 0) {
+			return &bucket->pairs[i];
+		}
+	}
+
+	return NULL;
+}
+
+const struct kn_value_t *kn_env_get(const char *identifier) {
+	struct kn_env_bucket_t *bucket = get_bucket(identifier);
+	struct kn_env_pair_t *pair = get_pair(bucket, identifier);
+
+	return pair == NULL ? NULL : &pair->value;
+}
+
+void kn_env_set(const char *identifier, struct kn_value_t value) {
+	struct kn_env_bucket_t *bucket = get_bucket(identifier);
+	struct kn_env_pair_t *pair = get_pair(bucket, identifier);
+
+	if (pair != NULL) {
+		kn_value_free(&pair->value);
+		pair->value = value;
+		return;
+	}
+
+	if (bucket->length == bucket->capacity) {
+		assert(bucket->capacity != 0);
+
+		bucket->capacity *= 2;
+		bucket->pairs = xrealloc(
+			bucket->pairs,
+			sizeof(struct kn_env_pair_t) * bucket->capacity
+		);
+	}
+
+	bucket->pairs[bucket->length] = (struct kn_env_pair_t) {
+		.name = strdup(identifier),
+		.value = value
+	};
+	++bucket->length;
+}
+
+
+#if 0
 /*
  * The type that holds all known variables within Knight.
  * 
@@ -58,7 +160,7 @@ const struct kn_value_t *kn_env_get(const char *identifier) {
 	exit(19);
 	return NULL;
 }
-const struct kn_value_t *kn_env_get1(const char *identifier) {
+const struct kn_value_t *kn_env_get(const char *identifier) {
 	assert(identifier != NULL);
 
 	for (size_t idx = 0; idx < ENV.length; ++idx) {
@@ -80,7 +182,7 @@ static void reallocate() {
 
 void kn_env_set(const char *identifier, struct kn_value_t value) {
 	assert(identifier != NULL);
-	struct kn_value_t *prev = (struct kn_value_t *) kn_env_get1(identifier);
+	struct kn_value_t *prev = (struct kn_value_t *) kn_env_get(identifier);
 
 	if (prev != NULL) {
 		kn_value_free(prev);
@@ -99,3 +201,4 @@ void kn_env_set(const char *identifier, struct kn_value_t value) {
 
 	++ENV.length;
 }
+#endif
